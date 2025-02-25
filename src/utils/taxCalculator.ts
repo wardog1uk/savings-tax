547b75b0fc300d0da94f1a80d02c06a36dba2412
wardog1uk <wardog1uk@users.noreply.github.com>
2025-02-25 12:40:54 +0000

    Handle the tax bands

--- src/utils/taxCalculator.ts
+++ src/utils/taxCalculator.ts
@@ -9,6 +9,22 @@ export function calculateStartingRate(otherIncome: number): number {
   return TAX_THRESHOLDS.STARTING_SAVINGS_RATE_LIMIT;
 }
 
+// Calculate the personal savings allowance for the tax band
+export function calculateAllowance(otherIncome: number): number {
+  if (otherIncome > TAX_THRESHOLDS.HIGHER_RATE_LIMIT) {
+    // Additional rate tax band
+    return PSA_ALLOWANCES.ADDITIONAL;
+  }
+  else if (otherIncome > TAX_THRESHOLDS.BASIC_RATE_LIMIT) {
+    // Higher rate tax band
+    return PSA_ALLOWANCES.HIGHER;
+  }
+  else {
+    // Basic rate tax band
+    return PSA_ALLOWANCES.BASIC;
+  }
+}
+
 // Calculate the tax on savings income given the other income
 export function calculateTax(savingsIncome: number, otherIncome: number): TaxCalculation {
   // Calculate how much of the personal savings allowance is remaining
@@ -17,16 +33,30 @@ export function calculateTax(savingsIncome: number, otherIncome: number): TaxCal
   // Get the starting savings rate
   let rate = calculateStartingRate(otherIncome);
 
-  // Set the personal savings allowance
-  let allowance = PSA_ALLOWANCES.BASIC;
+  // Get the personal savings allowance
+  let allowance = calculateAllowance(otherIncome);
 
   // Calculate the taxable amount
   let taxableAmount = Math.max(savingsIncome - remaining - rate - allowance, 0);
 
   // Calculate the tax due
-  let basicRateTax = taxableAmount * TAX_RATES.BASIC;
+  let basicRateTax = 0;
   let higherRateTax = 0;
   let additionalRateTax= 0;
+
+  // Calculate the tax to be paid
+  if (otherIncome > TAX_THRESHOLDS.HIGHER_RATE_LIMIT) {
+    // Additional rate tax band
+    additionalRateTax = taxableAmount * TAX_RATES.ADDITIONAL;
+  }
+  else if (otherIncome > TAX_THRESHOLDS.BASIC_RATE_LIMIT) {
+    // Higher rate tax band
+    higherRateTax = taxableAmount * TAX_RATES.HIGHER;
+  }
+  else {
+    // Basic rate tax band
+    basicRateTax = taxableAmount * TAX_RATES.BASIC;
+  }
 
   return {
     personalSavingsAllowance: allowance,
